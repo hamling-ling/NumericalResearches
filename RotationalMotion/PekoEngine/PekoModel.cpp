1fb70bab692698a6cc6aaae134edba97ffba41b0
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2014-01-15 02:18:39 +0900

    implemented bouncing but steady-state is unstable(keep moving)

--- RotationalMotion/PekoEngine/PekoModel.cpp
+++ RotationalMotion/PekoEngine/PekoModel.cpp
@@ -33,27 +33,50 @@ namespace pekomodel {
 		_theta = ZEROVEC3D;
 		_omega = ZEROVEC3D;
 		_t = 0;
-		_v = MODELVEC3DMake(0.0, 10.0, 0.0);;
-		_x = ZEROVEC3D;
+		_v = MODELVEC3DMake(0.0, 0.0, 0.0);
+		_x = MODELVEC3DMake(0.0, 0.5, 0.0);
 
 		memset(_points, 0, sizeof(_points));
 
 		_points[0].x = -0.5f;
-		_points[0].y = -0.5f;
+		_points[0].y = 0.0f;
 		_points[1].x = 0.5f;
-		_points[1].y = -0.5f;
+		_points[1].y = 0.0f;
 		_points[2].x = 0.5f;
-		_points[2].y = 0.5f;
+		_points[2].y = 1.0f;
 		_points[3].x = -0.5f;
-		_points[3].y = 0.5f;
+		_points[3].y = 1.0f;
 	}
 
 	void PekoModel::GetNext()
 	{
+		MODELVEC3D Fex = ZEROVEC3D;
+		MODELVEC3D Tex = ZEROVEC3D;
+		if(_t == 0.0) {
+			Fex.y = 350.0;
+			Tex = MODELVEC3DMake(0.0, 0.0, 10);
+		}
+
+		MODELVEC3D Ftot = ZEROVEC3D;
+		MODELVEC3D Ttot = ZEROVEC3D;
+		int hitPointIndex = 0;
+		bool hit = false;
+		for(int i = 0; i < MODEL_POINTS_LEN; i++) {
+			if(_points[i].y < 0.0) {
+				hit = true;
+				hitPointIndex = i;
+				break;
+			}
+		}
+
 		_t += dt;
 
-		MODELVEC3D Ftot = G;
-		MODELVEC3D Tau = MODELVEC3DMake(0.0, 0.0, 1);
+		AddVec(&G, &Fex, &Ftot);
+		if(hit) {
+			MODELVEC3D Ftot_old = Ftot;
+			_v.y = -_v.y;
+			_v.y = _v.y - _v.y*0.2; // decay
+		}
 
 		// dv/dt = F/m -> v = F/m * dt + v-
 		MODELVEC3D Vnxt;
@@ -65,9 +88,27 @@ namespace pekomodel {
 		ScaleVec(dt, &Vnxt, &Xnxt); 
 		AddVec(&Xnxt, &_x, &Xnxt);
 
+		if(hit) {
+			// w = (rxv)/r^2
+			MODELVEC3D r = ZEROVEC3D;
+			SubtVec(&_points[hitPointIndex], &_x, &r);
+			MODELVEC3D dx = ZEROVEC3D;
+			SubtVec(&Xnxt, &_x, &dx);
+			MODELVEC3D v = ZEROVEC3D;
+			ScaleVec(1.0/dt, &dx, &v);
+			MODELVEC3D rxv = ZEROVEC3D;
+			CrossProduct(&r, &v, &rxv);
+			ScaleVec(1.0/pow(Magnitude(&r), 2.0), &rxv, &_omega);
+
+			MODELVEC3D decay = ZEROVEC3D;
+			ScaleVec(-0.2, &_omega, &decay);// decay
+			SubtVec(&_omega, &decay, &_omega);
+		}
+
 		// domega/dt = tau/I -> w = tau/I * dt + w-
 		MODELVEC3D Wnxt;
-		ScaleVec(dt, &Tau, &Wnxt);
+		AddVec(&Ttot, &Tex, &Ttot);
+		ScaleVec(dt, &Ttot, &Wnxt);
 		AddVec(&Wnxt, &_omega, &Wnxt);
 
 		// dtheta/dt = omega -> theta = omega * dt + theta-
@@ -80,14 +121,13 @@ namespace pekomodel {
 		SubtVec(&Wnxt, &_omega, &domega);
 		SubtVec(&Theta, &_theta, &dtheta);
 
-		for(int i = 0; i < MODEL_POINTS_LEN; i++)
-		{
+		for(int i = 0; i < MODEL_POINTS_LEN; i++) {
 			AddVec(&(_points[i]), &dx, &(_points[i]));
 
 			MODELVEC3D cg2pt, cg2ptrot;
 			SubtVec(&(_points[i]), &Xnxt, &cg2pt);
-			RotateAroundZ(&cg2pt, dtheta.z, &cg2pt);
-			AddVec(&Xnxt, &cg2pt, &(_points[i]));
+			RotateAroundZ(&cg2pt, dtheta.z, &cg2ptrot);
+			AddVec(&Xnxt, &cg2ptrot, &(_points[i]));
 		}
 
 		_v = Vnxt;
