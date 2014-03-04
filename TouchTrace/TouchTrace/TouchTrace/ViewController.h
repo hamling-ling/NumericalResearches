3d15ecb1af8c4aae6f105df8fa22ed1ed32d7b1e
Nobuhiro Kuroiwa <nobu@Nobuhiro-no-MacBook-Pro.local>
2014-03-05 01:24:35 +0900

    added test app

---
+++ TouchTrace/TouchTrace/TouchTrace/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TouchTrace
+//
+//  Created by Nobuhiro Kuroiwa on 2014/03/05.
+//  Copyright (c) 2014年 Nobuhiro Kuroiwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TouchTrace/TouchTrace/TouchTraceTests/TouchTraceTests.m
@@ -0,0 +1,34 @@
+//
+//  TouchTraceTests.m
+//  TouchTraceTests
+//
+//  Created by Nobuhiro Kuroiwa on 2014/03/05.
+//  Copyright (c) 2014年 Nobuhiro Kuroiwa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouchTraceTests : XCTestCase
+
+@end
+
+@implementation TouchTraceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
