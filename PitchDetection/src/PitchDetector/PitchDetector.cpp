2a7ea79982984bcc137a6abb7a66d57b37948e89
hamling_ling <tacobell@gj9.so-net.ne.jp>
2015-03-30 20:30:28 +0900

    fixed wrong result bug

--- PitchDetection/src/PitchDetector/PitchDetector.cpp
+++ PitchDetection/src/PitchDetector/PitchDetector.cpp
@@ -1,11 +1,14 @@
 #include "PitchDetector.h"
 
 #include <iostream>
+#include <cmath>
 #include "NsdfCompute/AutoCorrelation.h"
 #include "NsdfWalker/NsdfWalker.h"
 
 using namespace std;
 using namespace osakanaengine;
+
+static const double kNoteConst = log10(pow(2.0, 1.0 / 12.0));
 
 PitchDetector::PitchDetector(int samplingRate, int samplingSize)
 : 
@@ -82,13 +85,14 @@ bool PitchDetector::ComputeNsdf(double* x)
 		_x2[t] = x[t] * x[t];
 
 		// debug
-		std::cout << "r[" << t << "]=" << _r[t] << endl;
+		//std::cout << "r[" << t << "]=" << _r[t] << endl;
+		//std::cout << "x2[" << t << "]=" << _x2[t] << endl;
 	}
 
 	// m(t)
 	memset(_m, 0, sizeof(double)* N);
 	for (int t = 0; t < N; t++) {
-		for (int j = 0; j < N; j++) {
+		for (int j = 0; j < N - t; j++) {
 			_m[t] = _m[t] + _x2[j] + _x2[j + t];
 		}
 	}
@@ -98,7 +102,7 @@ bool PitchDetector::ComputeNsdf(double* x)
 		_nsdf[t] = 2.0 * _r[t] / _m[t];
 
 		// debug
-		std::cout << "nsdf[" << t << "]=" << _nsdf[t] << endl;
+		//std::cout << "nsdf[" << t << "]=" << _nsdf[t] << endl;
 	}
 
 	return true;
@@ -118,8 +122,14 @@ int PitchDetector::AnalyzeNsdf()
 		return -1;
 	}
 
-	double freq = keyMaxs[0].index / _samplingRate;
-	// t.b.d
+	if (keyMaxs[0].index == 0.0) {
+		return -1;
+	}
 
-	return 1;
+	double freq = _samplingRate/keyMaxs[0].index;
+
+	double dnote = log10(freq / 27.5) / kNoteConst;
+	int inote = static_cast<int>(0.5 + dnote);
+
+	return inote;
 }
