56fc126a46ea52800499c89bd8c5e27bd10efdfd
hamling_ling <tacobell@gj9.so-net.ne.jp>
2015-03-31 13:22:05 +0900

    fix builderror again

--- PitchDetection/src/MsVC/PitchDetector/SampleApp/stdafx.h
+++ PitchDetection/src/MsVC/PitchDetector/SampleApp/stdafx.h
@@ -5,10 +5,12 @@
 
 #pragma once
 
+#if defined (WIN32)
 #include "targetver.h"
 
 #include <stdio.h>
 #include <tchar.h>
+#endif
 
 
 
--- PitchDetection/src/MsVC/PitchDetector/SampleApp/targetver.h
+++ PitchDetection/src/MsVC/PitchDetector/SampleApp/targetver.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#if defined (__WIN32__)
 // SDKDDKVer.h をインクルードすると、利用できる最も上位の Windows プラットフォームが定義されます。
 
 // 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
 // SDKDDKVer.h をインクルードする前に、サポート対象とするプラットフォームを示すように _WIN32_WINNT マクロを設定します。
 
 #include <SDKDDKVer.h>
-#endif
